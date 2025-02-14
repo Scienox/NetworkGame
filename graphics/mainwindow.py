d6d315ba6386f8215f0bca1fef696b467baa5248
Bex Jonathan <bexjonathan777@gmail.com>
2025-02-14 23:07:46 +0100

    add home page UI

--- graphics/mainwindow.py
+++ graphics/mainwindow.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit
+from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QPushButton, QLineEdit, QStackedWidget
 
 
 class MainWindow(QMainWindow):
@@ -9,16 +9,47 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(QWidget(parent=self))
 
+        self.myStyleSheet()
+
         self.__buildCentralWidget()
 
         self.__menuBar()
 
     def __buildCentralWidget(self):
-        self.layoutCentralWidget = QHBoxLayout(self.centralWidget())
+        self.layoutCentralWidget = QVBoxLayout(self.centralWidget())
+        self.homeButton = QPushButton(text="<-", parent=self.centralWidget())
+
+        self.stackedWidget = QStackedWidget(parent=self.centralWidget())
+        self.homeButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+        self.welcomePage = QWidget(parent=self.stackedWidget)
+        self.IpConfigPage = QWidget(parent=self.stackedWidget)
+        self.__buildWelcomePage()
+
+        self.stackedWidget.addWidget(self.welcomePage)
+        self.stackedWidget.addWidget(self.IpConfigPage)
+
+        self.layoutCentralWidget.addWidget(self.homeButton)
+        self.layoutCentralWidget.addStretch(1)
+        self.layoutCentralWidget.addWidget(self.stackedWidget)
+        self.layoutCentralWidget.addStretch(30)
+
+
+        """self.layoutCentralWidget = QHBoxLayout(self.centralWidget())
         self.layoutCentralWidget.setParent(self.centralWidget())
         self.widgetIp = QWidget(self.centralWidget())
         self.layoutCentralWidget.addWidget(self.widgetIp)
-        self.__buildWidgetiP()
+        self.__buildWidgetiP()"""
+
+    def __buildWelcomePage(self):
+        self.layoutWelcomePageGrid = QGridLayout(self.welcomePage)
+        self.buttonPageIpconfig = QPushButton(parent=self.welcomePage, text="IpConfig")
+        self.buttonPageIpconfig.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.buttonPageBinaryinfo = QPushButton(parent=self.welcomePage, text="Ip binaire")
+        self.buttonPageVlsm = QPushButton(parent=self.welcomePage, text="VLSM")
+
+        self.layoutWelcomePageGrid.addWidget(self.buttonPageIpconfig, 0, 0)
+        self.layoutWelcomePageGrid.addWidget(self.buttonPageBinaryinfo, 0, 1)
+        self.layoutWelcomePageGrid.addWidget(self.buttonPageVlsm, 1, 0, 1, 2)
 
     def __buildWidgetiP(self):
         self.layoutWidgetIp = QHBoxLayout(self.widgetIp)
@@ -45,3 +76,25 @@ class MainWindow(QMainWindow):
     def __menuBar(self):
         pass
 
+    def myStyleSheet(self):
+        stylesheet = """
+            * {
+                background-color: #21252c;
+                color: #acacac;
+                font-family: Arial, sans-serif;
+                font-size: 14px;
+            }
+            QWidget {
+                
+            }
+            QPushButton {
+                
+            }
+            QPushButton:hover {
+                
+            }
+            QLabel {
+                
+            }
+            """
+        self.setStyleSheet(stylesheet)
