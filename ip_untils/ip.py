1a69899fa22be6216db9beee35ce1a12c6418063
Bex Jonathan <bexjonathan777@gmail.com>
2025-03-31 13:38:33 +0200

    ip contains and iter added

--- ip_untils/ip.py
+++ ip_untils/ip.py
@@ -55,12 +55,20 @@ class IP:
         else:
             return NotImplemented
         
+    def __iter__(self):
+        referenceIp = IP(self.network, self.cidr)
+        hostBits = referenceIp.get_bitsHost()
+        for _ in range(2**len(hostBits)):
+            yield referenceIp.ipHost
+            addBool(hostBits)
+
     def __contains__(self, otherIp):
         if isinstance(otherIp, IP):
-            if self.network == otherIp.network:
-                if self.subMask == otherIp.subMask:
-                    return True
-            return False
+            if self.cidr <= otherIp.cidr:
+                for address in self:
+                    if otherIp.network == address:
+                        return True
+                return False
         else:
             return NotImplemented
 
@@ -70,7 +78,9 @@ class IP:
 
     @property
     def ipHost(self):
-        self.ipHost_ = [octet for octet in self.convertToString(self.ipHostBinary).split(".")]
+        decimal = self.convertToDecimal(self.ipHostBinary)
+        sequence = self.convertToString(decimal)
+        self.ipHost_ = [int(octet) for octet in sequence.split(".")]
         return self.convertToString(self.ipHost_)
     
     @property
@@ -113,7 +123,7 @@ class IP:
     def totalHost(self):
         return self.totalHost_
     
-    def get_hostBinary(self):
+    def get_bitsHost(self):
         bitCurrent = 1
         host = []
         for octet in range(4):
@@ -122,6 +132,16 @@ class IP:
                     host.append(self.ipHostBinary[octet][bit])
                 bitCurrent += 1
         return host
+    
+    def get_bitsNetwork(self):
+        bitCurrent = 1
+        network = []
+        for octet in range(4):
+            for bit in range(8):
+                if bitCurrent <= self.cidr:
+                    network.append(self.ipHostBinary[octet][bit])
+                bitCurrent += 1
+        return network
 
     def convertToBinary(self, vector):
         matrixBinary = [[Bit() for bit in range(8)] for _ in range(4)]
@@ -253,46 +273,7 @@ class IP:
             return "E"
 
     def get_reservation(self):
-        if self.classIP == "A":  # ok
-            if 9 < self.network_[0] < 11:
-                if self.cidr < 8:
-                    return "mixed"
-                return "private"
-            else:
-                return "public"
-        elif self.classIP == "None":
-            return "localhost"
-        elif self.classIP == "B":
-            if (171 < self.network_[0] < 173) and (15 < self.network_[1] < 32):
-                return "private"
-            else:
-                if self.cidr < 12:
-                    return "mixed"
-                return "public"
-        elif self.classIP == "C":
-            if (self.network == "192.0.2.0") and (self.cidr == 24):
-                self.name_ = "(TEST-NET-1)"
-                return "IETF"
-            elif (self.network == "198.51.100.0") and (self.cidr == 24):
-                self.name_ = "(TEST-NET-2)"
-                return "IETF"
-            elif ("203.0.113.0" == self.network) and (self.cidr == 24):
-                self.name_ = "(TEST-NET-3)"
-                return "IETF"
-            elif (191 < self.network_[0] < 193) and (167 < self.network_[1] < 169):
-                if self.cidr < 16:
-                    return "mixed"
-                return "private"
-            else:
-                if self.cidr < 13:
-                    return "mixed"
-                return "public"
-        elif self.classIP == "D":
-            if self.cidr < 4:
-                raise ValueError("Multicast exceeded\nMinimal cidr is /4\n")
-            return "multicast"
-        else:
-            return "None"
+        return Reservation(self)._detectReservation()
 
     def type_detect(self):
         if self.ipHost == self.network:
@@ -320,4 +301,102 @@ class Bit:
             self._value = value
         else:
             raise ValueError("Value is an int from 0 to 1")
+
+
+class Reservation:
+    def __init__(self, Ip:IP):
+        self._networkIp = Ip.network_
+        self._classIp = Ip.classIP
+        self._cidrIp = Ip.cidr
+
+    @property
+    def networkIp(self):
+        return self._networkIp
+
+    @property
+    def classIp(self):
+        return self._classIp
+    
+    @property
+    def cidr(self):
+        return self._cidrIp
+    
+    def getMinimalCidr(self):
+        if self.classIp == "A":
+            return 8, 8
+        elif self.classIp == "B":
+            return 16, 12
+        elif self.classIp == "C":
+            return 24, 16
+        else:
+            return 4, 4
+
+    @property  
+    def isBadCidrForPrivate(self):
+        privateCidr, _ = self.getMinimalCidr()
+        return self.cidr < privateCidr
+    
+    @property
+    def isBadCidrForPublic(self):
+        _, publicCidr = self.getMinimalCidr()
+        return self.cidr < publicCidr
+
+    def _detectReservation(self):
+        if self.classIp == "A":  # ok
+            if 9 < self.networkIp[0] < 11:
+                if self.isBadCidrForPrivate:
+                    return "mixed"
+                return "private"
+            if self.isBadCidrForPublic:
+                return "mixed"
+            return "public"
         
+        elif self.classIp == "None":
+            return "localhost"
+        
+        elif self.classIp == "B":
+            if (171 < self.networkIp[0] < 173) and (15 < self.networkIp[1] < 32):
+                if self.isBadCidrForPrivate:
+                    return "mixed"
+                return "private"
+            if self.isBadCidrForPublic:
+                return "mixed"
+            return "public"
+        
+        elif self.classIp == "C":
+            if (self.networkIp == [192, 0, 2, 0]) and (self.cidr == 24):
+                #self.name_ = "(TEST-NET-1)"
+                return "IETF"
+            elif (self.networkIp == [198, 51, 100, 0]) and (self.cidr == 24):
+                #self.name_ = "(TEST-NET-2)"
+                return "IETF"
+            elif (self.networkIp == [203, 0, 113, 0]) and (self.cidr == 24):
+                #self.name_ = "(TEST-NET-3)"
+                return "IETF"
+            
+            elif (191 < self.networkIp[0] < 193) and (167 < self.networkIp[1] < 169):
+                if self.isBadCidrForPrivate:
+                    return "mixed"
+                return "private"
+            elif self.isBadCidrForPublic:
+                return "mixed"
+            return "public"
+        
+        elif self.classIp == "D":
+            if self.isBadCidrForPrivate:
+                raise ValueError("Multicast exceeded\nMinimal cidr is /4\n")
+            return "multicast"
+        else:
+            return "None"
+
+
+def addBool(sequence, current=-1):
+    target:Bit = sequence[current]
+    if target.value == 0:
+        sequence[current].value = 1
+        return
+    elif all(bit.value == 1 for bit in sequence):
+        return
+    else:
+        sequence[current].value = 0
+        return addBool(sequence, current-1)
